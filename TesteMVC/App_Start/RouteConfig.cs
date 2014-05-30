23834238adb649e54f842667e46afb3378bb07f7
alexadre1000a <alexadre1000a@hotmail.com>
2014-05-30 18:57:05 -0400

    modificação

--- TesteMVC/App_Start/RouteConfig.cs
+++ TesteMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TesteMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ValidarUsuario", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
