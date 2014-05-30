b1775eac734e5193e1bdf7ca3655b6fb85ce7aa1
alexadre1000a <alexadre1000a@hotmail.com>
2014-05-30 18:18:33 -0400

    Atualização Site

---
+++ TesteMVC/Models/AutorizaUsuarioLogado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Providers.Entities;
+
+namespace TesteMVC.Models
+{
+    public class AutorizaUsuarioLogado : AuthorizeAttribute
+    {
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            base.OnAuthorization(actionContext);
+          
+        }
+    }
+}
